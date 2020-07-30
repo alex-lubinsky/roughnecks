feea272f078fea0594102315133dc2be08645e4d
Alex Lubinsky <alex.lubinsky@gmail.com>
2020-07-30 10:54:10 -0400

    changed some settings for deployment

--- src/components/DisplayMissionPage.js
+++ src/components/DisplayMissionPage.js
@@ -4,48 +4,90 @@ import { startSetMissions } from "../actions/missions";
 import TransactionsTable from "./TransactionsTable";
 import { startSetCharacters } from "../actions/characters";
 import { startSetTransactions } from "../actions/transactions";
+// import Button from 'react-bootstrap/Button'
+// import Modal from "react-bootstrap/Modal";
 
 class DisplayMissionPage extends React.Component {
+  
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     showItemModal:false
+  //   };
+  // }
+  
   componentDidMount() {
     this.props.startSetMissions();
     this.props.startSetCharacters();
     this.props.startSetTransactions();
   }
 
+  // handleItemModalClose =() => {
+  //   this.setState({showItemModal: false})
+  // }
+
+  // handleItemModalOpen =() => {
+  //   this.setState({showItemModal: true})
+  // }
+
+  // onGiveItemClick = () => {
+
+  // }
+
   render() {
     const dm = this.props.characters.find(
       (character) => character.id === this.props.mission.dm
     );
+
     return (
-      <div className="div-margin-sm">
-        {this.props.transactionsIsLoading ||
-        this.props.missionsIsLoading ||
-        this.props.charactersIsLoading ? null : (
-          <div>
-            <h1>{`Episode ${this.props.mission.episode}: ${this.props.mission.name}`}</h1>
-            <p>Played on: {this.props.mission.playedOn}</p>
-            <p>
-              DM: {dm.firstName} {dm.lastName}
-            </p>
-            Players:{" "}
-            <ul>
-            {this.props.mission.characters.map((pc) => {
-              const character = this.props.characters.find(
-                (character) => character.id === pc
-              );
-              return <li key={character.id}>{`${character.firstName} ${character.lastName}`}</li>;
-            })}
-            </ul>
-            <TransactionsTable
-              transactions={this.props.transactions.filter(
-                (transaction) => transaction.mission === this.props.mission.id
-              )}
-              characters={this.props.characters}
-              missions={this.props.missions}
-            />
-          </div>
-        )}
-      </div>
+      <>
+        {/* <Modal show={this.state.showItemModal}  onHide={this.handleItemModalClose}>
+          <Modal.Header closeButton>
+            <h1>Give a PC an Item</h1>
+          </Modal.Header>
+          <Modal.Body>
+          
+          </Modal.Body>
+          <Modal.Footer>
+            <Button>Give Item</Button>
+          </Modal.Footer>
+        </Modal> */}
+
+        <div className="div-margin-sm">
+          {this.props.transactionsIsLoading ||
+          this.props.missionsIsLoading ||
+          this.props.charactersIsLoading ? null : (
+            <div>
+              <h1>{`Episode ${this.props.mission.episode}: ${this.props.mission.name}`}</h1>
+              {/* <span>
+                {dm.creator === this.props.user.id || this.props.user.is_staff === true ? 
+                  <Button onClick={this.handleItemModalOpen}>Give PC an Item</Button> : null}
+              </span> */}
+              <p>Played on: {this.props.mission.playedOn}</p>
+              <p>
+                DM: {dm.firstName} {dm.lastName}
+              </p>
+              Players:{" "}
+              <ul>
+              {this.props.mission.characters.map((pc) => {
+                const character = this.props.characters.find(
+                  (character) => character.id === pc
+                );
+                return <li key={character.id}>{`${character.firstName} ${character.lastName}`}</li>;
+              })}
+              </ul>
+              <TransactionsTable
+                transactions={this.props.transactions.filter(
+                  (transaction) => transaction.mission === this.props.mission.id
+                )}
+                characters={this.props.characters}
+                missions={this.props.missions}
+              />
+            </div>
+          )}
+        </div>
+      </>
     );
   }
 }
@@ -63,6 +105,8 @@ const mapStateToProps = (state, props) => ({
   missions: state.missions.data,
   characters: state.characters.data,
   transactions: state.transactions.data,
+  user: state.auth.user,
+  
   missionsIsLoading: state.missions.isLoading,
   charactersIsLoading: state.characters.isLoading,
   transactionsIsLoading: state.transactions.isLoading,
