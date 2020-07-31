e83c63804269e3de83db173ea182a997fe73e838
Alex Lubinsky <alex.lubinsky@gmail.com>
2020-07-31 16:58:19 -0400

    added code for ico file to work

--- roughnecks/urls.py
+++ roughnecks/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls import url
 from userapi.views import UserAPIView
 from characters.views import FrontendAppView
+
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +31,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('reset/<uidb64>/<token>/', FrontendAppView.as_view(), name="password_reset_confirm"),
     path('api/auth/', include('userapi.urls')),
+    re_path(r'^favicon\.ico$', favicon_view),
     re_path('^', FrontendAppView.as_view()),
 ]
