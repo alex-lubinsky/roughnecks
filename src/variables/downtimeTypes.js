0ae318f078539d1b26e31f6ae47c094305990543
Alex Lubinsky <alex.lubinsky@gmail.com>
2020-08-27 17:12:43 -0400

    adding file

---
+++ src/variables/downtimeTypes.js
@@ -0,0 +1,10 @@
+export const DOWNTIME_TYPES = [
+  {id: 1, name: 'Training Room'},
+  {id: 2, name: 'Miscellaneous'},
+  {id: 3, name: 'The Job Board'},
+  {id: 4, name: 'Scounting'},
+  {id: 5, name: '	Materials Scavenging'},
+  {id: 6, name: '	Carlyle\'s Trading Network'},
+  {id: 7, name: 'Upgrading the Airship'},
+  {id: 8, name: 'Madame Lysalka\'s Tavern'},
+]
